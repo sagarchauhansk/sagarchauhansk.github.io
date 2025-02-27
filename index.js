27b5e855c82d06fb759fe377f7b3835eaf08bc61
sagarchauhansk <119773206+sagarchauhansk@users.noreply.github.com>
2025-02-27 22:21:02 +0530

    generated index.html

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
-app.use(express.static('public')); // Serve static files (CSS, JS)
+app.use(express.static('public')); // Serve static files
 
 const data = require('./data.json');
 
 app.get('/', (req, res) => {
-  res.render('index', { data });
+    res.render('index', { data });
+});
+
+app.get('/generate-html', (req, res) => {
+    res.render('index', { data }, (err, html) => {
+        if (err) {
+            return res.send('Error rendering EJS');
+        }
+        fs.writeFileSync('index.html', html);
+        res.send('index.html generated');
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log('Server running on http://localhost:3000');
 });
+
